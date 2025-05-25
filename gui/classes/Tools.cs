511169947a8a6e6a097019d035f13fedefdf79fe
Максим Ледовский <santech_montage@mail.ru>
2025-05-25 18:22:58 +0300

    Created class for tools to work with db

---
+++ gui/classes/Tools.cs
@@ -0,0 +1,16 @@
+﻿using db.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace gui.classes {
+
+    public class Tools {
+
+        public static List<string> GetTableNames<TContext>(TContext context) where TContext : DbContext {
+            return context.Model.GetEntityTypes()
+                .Select(e => e.GetTableName())
+                .Where(t => t != null)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
