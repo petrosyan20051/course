5d0593d25e0c6488241804ded932ab4f17c1fd53
Максим Ледовский <santech_montage@mail.ru>
2025-05-25 23:01:16 +0300

    cur commit

--- db/obj/Debug/net8.0/db.AssemblyInfo.cs
+++ db/obj/Debug/net8.0/db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec628583076ee220fcb9bee3799b006300ea4ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c0ba0563ba0b4b19bbfcf10454a8d92cd64930")]
 [assembly: System.Reflection.AssemblyProductAttribute("db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
