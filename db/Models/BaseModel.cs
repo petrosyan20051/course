af768ecc0a3ad0ac325e697d79f5738e41765bcb
Максим Ледовский <santech_montage@mail.ru>
2025-05-28 21:30:53 +0300

    added custome attribute so that inherited from BaseModel properties are not shown as leader. It means that it is hidden or going at the end when use MetaData

--- db/Models/BaseModel.cs
+++ db/Models/BaseModel.cs
@@ -1,11 +1,26 @@
-﻿namespace db.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using db.Tools;
+
+namespace db.Models {
 
     public abstract class BaseModel {
+
+        [DisplayPriority(false)]
         public string WhoAdded { get; set; } = string.Empty;
+
+        [DisplayPriority(false)]
         public DateTime WhenAdded { get; set; }
+
+        [DisplayPriority(false)]
         public string? WhoChanged { get; set; } = null;
+
+        [DisplayPriority(false)]
         public DateTime? WhenChanged { get; set; } = null;
+
+        [DisplayPriority(false)]
         public string? Note { get; set; } = null;
+
+        [DisplayPriority(false)]
         public DateTime? isDeleted { get; set; } = null;
     }
 }
--- db/obj/Debug/net8.0/db.AssemblyInfo.cs
+++ db/obj/Debug/net8.0/db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f1aff395757245217e7d542ab80d65fc79cf6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e15ddaaef4139897d1a6c648634b832d0ce6be")]
 [assembly: System.Reflection.AssemblyProductAttribute("db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
