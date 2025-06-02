fd2d4e99c7bb8059ec2cdf550147b46a5d1e9e03
santech_montage <santech_montage@mail.ru>
2025-06-02 20:05:33 +0300

    updated for local laptop server

--- db/obj/Debug/net8.0/db.AssemblyInfo.cs
+++ db/obj/Debug/net8.0/db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+615ac7871b19c7e9847d0923ada057e82f9e9702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037785144935b1b33f2c3dd7f28a2e137d82f95b")]
 [assembly: System.Reflection.AssemblyProductAttribute("db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
