d5203d65c4d3282ecf3f9afb6ec47fafb1696ab4
santech_montage <santech_montage@mail.ru>
2025-09-09 21:41:20 +0300

    Inteface update for repository

--- db/Interfaces/IRepository.cs
+++ db/Interfaces/IRepository.cs
@@ -2,8 +2,8 @@
     public interface IRepository<TEntity, TKey> : IRecovarable<TKey>, IDeletable<TKey> {
 
         // Async versions
-        Task<TEntity> GetByIdAsync(TKey id);
-        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task<IEnumerable<TEntity>?> GetAllAsync();
         Task AddAsync(TEntity entity);
         Task UpdateAsync(TEntity entity);
         // Some addictive async methods
