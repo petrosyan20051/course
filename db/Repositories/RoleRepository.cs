8c0a7ecf02aec48fb7d15c1c98d48d23b0b6ce6a
santech_montage <santech_montage@mail.ru>
2025-09-14 12:06:02 +0300

    Role repos add

---
+++ db/Repositories/RoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using db.Contexts;
+using db.Models;
+using Microsoft.EntityFrameworkCore;
+using TypeId = int;
+
+namespace db.Repositories {
+    public class RoleRepository {
+
+        private readonly OrderDbContext _context;
+
+        public RoleRepository(OrderDbContext context) {
+            _context = context;
+        }
+
+        public async Task<Role?> GetByIdAsync(TypeId id) {
+            return await _context.Roles
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
