f9680b8d911cd16d2ec7c5b9176c4e34165aa0cc
santech_montage <santech_montage@mail.ru>
2025-09-14 11:38:51 +0300

    DTO class add

---
+++ db/Models/LoginPrompt.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace db.Models {
+    public class LoginPrompt {
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        public required string Login {  get; set; }
+
+        [Required(ErrorMessage = "Пароль пользователя обязателен")]
+        public required string Password { get; set; }
+    }
+}
