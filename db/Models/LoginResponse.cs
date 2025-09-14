598eb3eb37ec5e9d79858e2c6a8c23f468b7ac57
santech_montage <santech_montage@mail.ru>
2025-09-14 12:06:59 +0300

    Login response class add

---
+++ db/Models/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using db.Interfaces;
+using TypeId = int;
+
+namespace db.Models {
+    public class LoginResponse {
+        public TypeId UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+
+        public IInformation.UserRights Rights { get; set; }
+
+        public bool CanGet { get; set; }
+        public bool CanPost { get; set; }
+        public bool CanUpdate { get; set; }
+        public bool CanDelete { get; set; }
+    }
+}
