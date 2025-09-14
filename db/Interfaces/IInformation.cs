34978e7d4abee8edf83d1ee91c0590b8d50991e4
santech_montage <santech_montage@mail.ru>
2025-09-14 13:42:18 +0300

    login + information update

--- db/Interfaces/IInformation.cs
+++ db/Interfaces/IInformation.cs
@@ -1,6 +1,7 @@
 ﻿namespace db.Interfaces {
     public interface IInformation {
         public enum UserRights { Basic, Editor, Admin } // rights access for user
+        public enum RegisterType { Anonymous, Admin } //  depends on who register new user
 
         public static string AppName = "Курсовая работа";
     }
--- db/Models/LoginPrompt.cs
+++ db/Models/LoginPrompt.cs
@@ -3,7 +3,7 @@
 namespace db.Models {
     public class LoginPrompt {
         [Required(ErrorMessage = "Имя пользователя обязательно")]
-        public required string Login {  get; set; }
+        public required string Login { get; set; }
 
         [Required(ErrorMessage = "Пароль пользователя обязателен")]
         public required string Password { get; set; }
