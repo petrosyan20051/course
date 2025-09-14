170e7439771b0a8a964ae43dad84b2250fa02363
santech_montage <santech_montage@mail.ru>
2025-09-14 13:34:29 +0300

    DTO for registration add

---
+++ db/Models/RegisterPrompt.cs
@@ -0,0 +1,18 @@
+﻿using db.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace db.Models {
+    public class RegisterPrompt {
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        public required string UserName { get; set; }
+
+        [Required(ErrorMessage = "Пароль пользователя обязателен")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Тип регистрации обязателен")]
+        public IInformation.RegisterType RegisterType { get; set; }
+
+        [Required(ErrorMessage = "Тип регистрируемого пользователя обязателен")]
+        public IInformation.UserRights RegisterRights { get; set; }
+    }
+}
---
+++ db/Models/RegisterResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace db.Models {
+    public class RegisterResponse {
+        public required string UserName { get; set; }
+
+        public required bool CanGet { get; set; }
+        public required bool CanPost { get; set; }
+        public required bool CanUpdate { get; set; }
+        public required bool CanDelete { get; set; }
+    }
+}
