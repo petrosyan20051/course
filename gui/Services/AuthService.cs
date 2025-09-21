6e80b587ae0ebbbf492abe63447f9861d28c24a5
santech_montage <santech_montage@mail.ru>
2025-09-21 15:55:41 +0300

    Authorization handling added. Checking rights added

--- gui/Services/AuthService.cs
+++ gui/Services/AuthService.cs
@@ -3,11 +3,15 @@ using DbAPI.DTO;
 using Microsoft.Extensions.Configuration;
 using System.Net.Http.Json;
 using System.Text.Json;
+using System.Net.Http.Headers;
 
 namespace gui.Services {
     public class AuthService : BaseApiService<Credential> {
         private readonly HttpClient _httpClient;
         private readonly string BaseUrl;
+        private string _token;
+
+        public bool IsAuthenticated { get; private set; }
 
         public const string EntityPath = "Credential/";
 
@@ -30,7 +34,7 @@ namespace gui.Services {
             // Enforce http client send JSON format response
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(
-                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public async Task<LoginResponse> LoginAsync(string username, string password) {
@@ -40,50 +44,64 @@ namespace gui.Services {
                 Password = password
             };
 
+            // Get response
             var response = await _httpClient.PostAsJsonAsync(EntityPath + "Login", loginPrompt);
-            response.EnsureSuccessStatusCode();
+            var loginResponse = await HandleResponseAsync<LoginResponse>(response);
+            
+            _token = loginResponse.Token; // set current session token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            IsAuthenticated = true;
 
-            return await response.Content.ReadFromJsonAsync<LoginResponse>();
+            return loginResponse;
+        }
+
+        public void LogOut() {
+            _token = null;
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+            IsAuthenticated = false;
         }
 
         public async Task<RegisterResponse> RegisterAsync(RegisterPrompt registerPrompt) {
             var response = await _httpClient.PostAsJsonAsync(EntityPath + "Register", registerPrompt);
             return await HandleResponseAsync<RegisterResponse>(response);
-            
-            /*response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadFromJsonAsync<RegisterResponse>();*/
         }
 
         public override async Task<IEnumerable<Credential>?> GetAllAsync() {
+            EnsureAuthorized();
             return await _httpClient.GetFromJsonAsync<IEnumerable<Credential>>(EntityPath + "GetAll");
         }
 
         public override async Task<Credential?> GetByIdAsync(int id) {
+            EnsureAuthorized();
             return await _httpClient.GetFromJsonAsync<Credential>(EntityPath + "GetById" + $"?id={id}");
         }
 
         public override async Task AddAsync(Credential entity) {
+            EnsureAuthorized();
             var response = await _httpClient.PostAsJsonAsync(EntityPath + "Add", entity);
             response.EnsureSuccessStatusCode();
         }
 
         public override async Task UpdateAsync(Credential entity) {
+            EnsureAuthorized();
             var response = await _httpClient.PutAsJsonAsync($"{EntityPath}UpdateById?id={entity.Id}", entity);
             response.EnsureSuccessStatusCode();
         }
 
         public override async Task DeleteAsync(int id) {
+            EnsureAuthorized();
             var response = await _httpClient.DeleteAsync(EntityPath + "Delete" + $"?id={id}");
             response.EnsureSuccessStatusCode();
         }
 
         public override async Task<bool> RecoverAsync(int id) {
+            EnsureAuthorized();
             var response = await _httpClient.GetAsync(EntityPath + "Recover" + $"?id={id}");
             return response.IsSuccessStatusCode;
         }
 
         public override async Task SoftDeleteAsync(int id) {
+            EnsureAuthorized();
             var response = await _httpClient.GetAsync(EntityPath + "SoftDelete" + $"?id={id}");
             response.EnsureSuccessStatusCode();
         }
@@ -101,6 +119,11 @@ namespace gui.Services {
             } catch (JsonException) {
                 throw new Exception($"Ошибка: {content}");
             }
+        }
+
+        private void EnsureAuthorized() {
+            if (!IsAuthenticated)
+                throw new UnauthorizedAccessException("Требуется авторизация. Войти в систему");
         }
     }
 }
