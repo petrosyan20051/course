eef72ea902fcb0030aca2bd2ba023707d73d6776
santech_montage <santech_montage@mail.ru>
2025-09-21 13:19:48 +0300

    Services added

---
+++ gui/Services/AuthService.cs
@@ -0,0 +1,105 @@
+﻿using System.Net.Http.Json;
+using db.Models;
+using DbAPI.DTO;
+using Microsoft.Extensions.Configuration;
+
+namespace gui.Services
+{
+    public class AuthService : BaseApiService<Credential>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string BaseUrl;
+
+        public const string EntityPath = "Credential/";
+
+        public AuthService()
+        {
+            // Use IP address from appconfig.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appconfig.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            BaseUrl = config.GetSection("ServerConfig:ServerIP").Value;
+            if (string.IsNullOrEmpty(BaseUrl))
+            {
+                throw new Exception("ServerIP не найден в конфигурации");
+            }
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+            // Enforce http client send JSON format response
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<LoginResponse> LoginAsync(string username, string password)
+        {
+            // Create prompt
+            var loginPrompt = new LoginPrompt
+            {
+                Login = username,
+                Password = password
+            };
+
+            // Try get access to api/Login
+            var response = await _httpClient.PostAsJsonAsync(EntityPath + "Login", loginPrompt);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<LoginResponse>();
+        }
+
+        public async Task<RegisterResponse> RegisterAsync(RegisterPrompt registerPrompt)
+        {
+            // Try get access to api/Register
+            var response = await _httpClient.PostAsJsonAsync(EntityPath + "Register", registerPrompt);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<RegisterResponse>();
+        }
+
+        public override async Task<IEnumerable<Credential>?> GetAllAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Credential>>(EntityPath + "GetAll");
+        }
+
+        public override async Task<Credential?> GetByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Credential>(EntityPath + "GetById" + $"?id={id}");
+        }
+
+        public override async Task AddAsync(Credential entity)
+        {
+            var response = await _httpClient.PostAsJsonAsync(EntityPath + "Add", entity);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public override async Task UpdateAsync(Credential entity)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{EntityPath}UpdateById?id={entity.Id}", entity);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public override async Task DeleteAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync(EntityPath + "Delete" + $"?id={id}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        public override async Task<bool> RecoverAsync(int id)
+        {
+            var response = await _httpClient.GetAsync(EntityPath + "Recover" + $"?id={id}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public override async Task SoftDeleteAsync(int id)
+        {
+            var response = await _httpClient.GetAsync(EntityPath + "SoftDelete" + $"?id={id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ gui/Services/BaseApiService.cs
@@ -0,0 +1,17 @@
+﻿using TKey = int;
+
+namespace gui.Services
+{
+    public abstract class BaseApiService<TEntity> where TEntity : class
+    {
+        protected readonly HttpClient _httpClient;
+
+        public abstract Task<TEntity?> GetByIdAsync(TKey id);
+        public abstract Task<IEnumerable<TEntity>?> GetAllAsync();
+        public abstract Task AddAsync(TEntity entity);
+        public abstract Task UpdateAsync(TEntity entity);
+        public abstract Task DeleteAsync(TKey id);
+        public abstract Task SoftDeleteAsync(TKey id);
+        public abstract Task<bool> RecoverAsync(TKey id);
+    }
+}
